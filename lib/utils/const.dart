8cbe16a5d79864812a9f09df6a76d2bb0cd2d06e
Abdallah Roshdy <67377062+roshdology123@users.noreply.github.com>
2023-08-07 16:40:49 +0300

    init e-learning

---
+++ lib/utils/const.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xffE4F1F8);
+const kSecondaryColor = Color(0xFF3787FF);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kBlackColor = Color(0xFF000000);
+
+const kMainTextColor = Color(0xFFFFFFFF);
+const kSecondaryTextColor = Color(0xFF3787FF);
+
+const kHeadlineTextColor = Color(0xFF060302);
+const kHintTextColor = Color(0xFF060302);
